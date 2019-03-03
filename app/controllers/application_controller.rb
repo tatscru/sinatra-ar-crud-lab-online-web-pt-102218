09b8eedbf3166d3ddfc0caac59b873f89df68549
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-03 19:03:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,44 @@
-
-require_relative '../../config/environment'
-
 class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+  
+  get '/' do 
+  end 
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
 
-  get '/' do
-  end
+   get '/articles/new' do
+    @article = Article.new
+    erb :new
+    
+  # post '/articles' do
+  #   Article.create(params[:title], content: params[:content])
+    
+  # end
+
+  # end
+
+  # get '/articles/:id/edit' do
+  #   @article = Article.find(params[:id])
+  #   erb :form
+  # end
+
+  # get '/posts/:id' do
+  #   @article = Article.find(params[:id])
+  #   erb :show
+  # end
+
+ 
+#   delete '/posts/:id/delete' do
+#     @post = Post.find(params[:id])
+#     @post.destroy
+#     erb :delete
+#   end
+
+#   patch '/posts/:id' do
+#     post = Post.find(params[:id])
+#     post.update(params[:post])
+#     redirect "/posts/#{post.id}"
+#   end
 end
