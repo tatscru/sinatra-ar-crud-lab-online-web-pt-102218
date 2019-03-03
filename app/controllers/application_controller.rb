b638a6a932c80a07e48e691522fb12e1adbb447b
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-03 19:29:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
 
   post '/articles' do
     @article = Article.create(params)
-    redirect to "/articles/:id "
+    redirect to "/articles/:id"
   end
 
   get '/articles/:id' do
