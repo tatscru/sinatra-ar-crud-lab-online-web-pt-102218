5e5b00e685cdb7c2a0a5636afea4a867dd17f267
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-03 19:38:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +7,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/' do 
   end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
 end
--- config.ru
+++ config.ru
@@ -5,8 +5,8 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
-
   run ApplicationController
+  
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
